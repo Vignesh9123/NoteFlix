d2b73aa348837c1746d36affa49f6a04baf0342a
Vignesh D <vignesh.d9123@gmail.com>
2024-12-25 08:04:24 +0530

    Refactor NotesListCard component to add tooltip for video timestamp link

--- src/components/cards/NotesListCard.tsx
+++ src/components/cards/NotesListCard.tsx
@@ -3,13 +3,24 @@ import { IUserNote, IVideoDetails } from '@/types'
 import { Checkbox } from '../ui/checkbox'
 import { secondsToTime } from '@/lib/utils';
 import Link from 'next/link';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip';
 function NotesListCard({note, videoDetails}: {note: IUserNote, videoDetails: IVideoDetails}) {
     const [todoCompleted, setTodoCompleted] = useState(note.todoCompleted);
   return (
     <>
     {note.category === "key point" && <div onClick={() => console.log("clicked")} key={note.text} className='flex flex-col gap-2 p-2 rounded-lg bg-muted dark:hover:bg-slate-900 duration-150 hover:scale-[1.01] cursor-pointer'>
     <li className='text-sm text-muted-foreground mt-2'>{note.text}</li>
+    <TooltipProvider >
+    <Tooltip >
+    <TooltipTrigger className='cursor-pointer inline w-fit'>
+
     <Link href={`https://www.youtube.com/watch?v=${videoDetails.youtubeId}&t=${note.timestamp}s`} target='_blank' onClick={(e) => e.stopPropagation()} className='text-sm text-white mt-2 bg-blue-500 rounded-md p-1 w-fit'>{secondsToTime(note.timestamp)}</Link>
+    </TooltipTrigger>
+    <TooltipContent>
+        <p>Go to video at {secondsToTime(note.timestamp)}</p>
+    </TooltipContent>
+    </Tooltip>
+    </TooltipProvider>
 </div>}
 {note.category === "todo" && <div onClick={() => console.log("clicked")} key={note.text} className='flex flex-col gap-2 p-2 rounded-lg bg-muted dark:hover:bg-slate-900 duration-150 hover:scale-[1.01] cursor-pointer'>
     <div className='text-sm flex items-center gap-2 text-muted-foreground mt-2'>
