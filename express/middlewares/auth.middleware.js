52775079fd8974fc9b4af7df594a0fd4dd2e0c4e
Vignesh D <vignesh.d9123@gmail.com>
2025-01-14 18:55:47 +0530

    Refactor authentication middleware to log the entire request object instead of just cookies for improved debugging

--- express/middlewares/auth.middleware.js
+++ express/middlewares/auth.middleware.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export async function authMiddleware(req, res, next) {
     try {
-        console.log(req.cookies);
+        console.log(req);
         const token = req.cookies.token;
         if (!token) {
             return res.status(401).json({ error: "Unauthorized" });
