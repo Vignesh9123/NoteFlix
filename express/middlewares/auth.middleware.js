8834834dc9ca72b115f20059e24ef778815111ae
Vignesh D <vignesh.d9123@gmail.com>
2025-01-14 18:51:18 +0530

    Enhance authentication middleware to log incoming cookies for debugging

--- express/middlewares/auth.middleware.js
+++ express/middlewares/auth.middleware.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export async function authMiddleware(req, res, next) {
     try {
+        console.log(req.cookies);
         const token = req.cookies.token;
         if (!token) {
             return res.status(401).json({ error: "Unauthorized" });
