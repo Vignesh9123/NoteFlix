df9514ed498053191699a1ada17400529c5c77b1
Vignesh D <vignesh.d9123@gmail.com>
2025-01-19 23:37:45 +0530

    Refactor credits reset logic to use async/await and improve API response
    - Changed the `shouldResetCredits` function to a synchronous function for better performance.

--- src/app/api/user/credits/route.ts
+++ src/app/api/user/credits/route.ts
@@ -1,9 +1,10 @@
 import User from "@/models/user.model";
 import connectDB from "@/dbConfig/connectDB";
 import { NextRequest, NextResponse } from "next/server";
+import { IUser } from "@/types";
 connectDB();
 
-const shouldResetCredits = async(createdAt: string) => {
+const shouldResetCredits = (createdAt: string) => {
     const now = new Date();
     const joinDate = new Date(createdAt);
     const yearsDiff = now.getFullYear() - joinDate.getFullYear();
@@ -16,11 +17,11 @@ export async function POST(req: NextRequest) {
     try {
         const users = await User.find({}).sort({ createdAt: -1 });
         const updatedUsers = users.filter((user) => shouldResetCredits(user.createdAt as string))
-        .map((user) => {
-            User.updateOne({ _id: user._id }, { $set: { creditsUsed: 0 } });
+        .map(async(user:IUser) => {
+            await User.updateOne({ _id: user._id }, { $set: { creditsUsed: 0 } });
         })
         await Promise.all(updatedUsers);
-        return NextResponse.json({ data: updatedUsers, message: `Credits reset successfully for ${updatedUsers.length} users` }, { status: 200 });
+        return NextResponse.json({ message: `Credits reset successfully for ${updatedUsers.length} users` }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
     }
